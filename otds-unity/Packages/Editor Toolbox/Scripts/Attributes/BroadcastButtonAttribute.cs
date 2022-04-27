185578fe9ad12152a11a45e7c45cc74e73915e23
Felipe Higino <43382352+felipe-higino@users.noreply.github.com>
2022-04-27 10:11:17 -0300

    project configuration
    project:
    - add .gitignore
    - folder structure and asset organization
    - add SetFolderStructure Script
    packages:
    - removed unecessary packages
    - add Art Assets CustomPackage
    - add Editor Toolbox CustomPackage
    - add TMPro
    - add InputSystem

---
+++ otds-unity/Packages/Editor Toolbox/Scripts/Attributes/BroadcastButtonAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    [Obsolete("Use EditorButtonAttribute instead.")]
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class BroadcastButtonAttribute : ButtonAttribute
+    {
+        public BroadcastButtonAttribute(string methodName, string label = null, ButtonActivityType type = ButtonActivityType.Everything) 
+            : base(methodName, label, type)
+        { }
+    }
+}
